c2ad17ca9fc35d55cac9da85aeb9ba7dbb2439fe
aleksandr.afanasiev <aleksandr.afanasiev@nytimes.com>
2017-10-30 23:47:11 +0300

    execute install task only if tmux needs update

--- molecule/default/tests/test_tmux.py
+++ molecule/default/tests/test_tmux.py
@@ -1,11 +1,8 @@
 def test_tmux_is_installed(host):
-	pkg=host.package("tmux")
-	assert pkg.is_installed
+    pkg = host.package("tmux")
+    assert pkg.is_installed
+
 
 def test_tmux_version(host):
-	tmux_version=host.run("tmux -V | cut -d ' ' -f 2")
-	assert tmux_version.stdout.startswith("2")
-
-def test_tmux_config_file_is_in_place(host):
-	config=host.file("~/.tmux.conf")
-	assert config.exists
+    tmux_version = host.run("tmux -V | cut -d ' ' -f 2")
+    assert tmux_version.stdout.startswith("2")
